ce89686a5c29a9e08dbe70831323a01250d48b3a
Akashyadav1997 <104287158+Akashyadav1997@users.noreply.github.com>
2025-09-15 19:53:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+var count = 0; 
+let btn = document.querySelector("#btn");
+let preview = document.querySelector("#preview");
+const incrementFxn = () =>{
+	alert(count);
+	count++;
+	preview.textContent = count;
+}
+btn.addEventListener("click", incrementFxn);
